998480179870f65ac6e3e7bf4dddeea783fb62da
sanjay giri <sanjaygir@gmail.com>
2020-05-19 12:39:14 +0545

    hello

--- grails-app/controllers/welcome/HomeController.groovy
+++ grails-app/controllers/welcome/HomeController.groovy
@@ -6,16 +6,7 @@ class HomeController {
 
     def index() {
 
-        try {
-            // ex: http://www.youtube.com/watch?v=Nj6PFaDmp6c
-            String url = "https://www.youtube.com/watch?v=C0DPdy98e4c";
-            // ex: "/Users/axet/Downloads"
-            String path = "/var/roadrace/upload"
-            VGet v = new VGet(new URL(url), new File(path));
-            v.download();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        render "Hello"
 
     }
 }
